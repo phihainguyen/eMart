fa9fa6b95227d19a1be62a1420fc718d7a4f9214
phihainguyen <phihai93@gmail.com>
2021-01-15 11:05:01 -0800

    address form

--- client/src/components/CheckoutForm/AddressForm.jsx
+++ client/src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
 
 const AddressForm = () => {
-	return <div>ADDRESS</div>;
+	const methods = useForm();
+	return (
+		<div>
+			<Typography>Shippding Address</Typography>
+			<FormProvider>
+				<form>
+					<Grid />
+				</form>
+			</FormProvider>
+		</div>
+	);
 };
 
 export default AddressForm;
