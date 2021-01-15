b2bd5d28ceb3fe9aea13a51f61188a648c01ab72
phihainguyen <phihai93@gmail.com>
2021-01-15 12:00:33 -0800

    shipping

--- client/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ client/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+
+import { commerce } from '../../../lib/commerce';
+
 const steps = [ 'Shipping address', 'Payment details' ];
 
-const Checkout = () => {
-	const [ activeStep, setActiveStep ] = useState(2);
-
+const Checkout = ({ cart }) => {
+	const [ activeStep, setActiveStep ] = useState(0);
+	const [ checkoutToken, setCheckoutToken ] = useState(null);
 	const classes = useStyles();
 
+	useEffect(() => {
+		const generateToken = async () => {
+			try {
+				const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+				console.log(token);
+				setCheckoutToken(token);
+			} catch (error) {}
+		};
+
+		generateToken();
+	}, []);
+
 	const Confirmation = () => <div>Confirmation</div>;
+
 	const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+
 	return (
 		<div>
 			<div className={classes.toolbar} />
