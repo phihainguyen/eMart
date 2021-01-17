6158d4a1a258142fecb9798185b6421196203c7b
phihainguyen <phihai93@gmail.com>
2021-01-17 14:05:36 -0800

    review page

--- client/src/components/CheckoutForm/AddressForm.jsx
+++ client/src/components/CheckoutForm/AddressForm.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 import FormInput from './FormInput';
+import { Link } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
-const AddressForm = ({ checkoutToken }) => {
+
+const AddressForm = ({ checkoutToken, next }) => {
 	const [ shippingCountries, setShippingCountries ] = useState([]);
 	const [ shippingCountry, setShippingCountry ] = useState('');
 	const [ shippingSubdivisions, setShippingSubdivisions ] = useState([ '' ]);
@@ -74,14 +76,19 @@ const AddressForm = ({ checkoutToken }) => {
 				Shipping Address
 			</Typography>
 			<FormProvider {...methods}>
-				<form onSubmit="">
+				<form
+					onSubmit={methods.handleSubmit((data) =>
+						next({ ...data, shippingCountry, shippingSubdivision, shippingOption })
+					)}
+				>
 					<Grid container spacing={3}>
+						<FormInput name="firstName" label="First name" />
 						<FormInput name="lastName" label="Last name" />
 						<FormInput name="address1" label="Address" />
 						<FormInput name="email" label="Email" />
 						<FormInput name="City" label="City" />
 						<FormInput name="zip" label="ZIP / Postal code" />
-						<FormInput name="firstName" label="First name" />
+
 						<Grid item xs={12} sm={6}>
 							<InputLabel>
 								Shipping Country
@@ -132,6 +139,15 @@ const AddressForm = ({ checkoutToken }) => {
 						</Grid>
 					</Grid>
 				</form>
+				<br />
+				<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+					<Button component={Link} to="/cart" variant="outlined">
+						Back to Cart
+					</Button>
+					<Button type="submit" color="primary" variant="contained" onClick={next}>
+						Next
+					</Button>
+				</div>
 			</FormProvider>
 		</div>
 	);
--- client/src/components/CheckoutForm/PaymentForm.jsx
+++ client/src/components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
-const PaymentForm = () => {
-	return <div>PAYMENT</div>;
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review';
+const PaymentForm = ({ checkoutToken }) => {
+	return (
+		<div>
+			<Review checkoutToken={checkoutToken} />
+		</div>
+	);
 };
 
 export default PaymentForm;
